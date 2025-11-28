de6e2b0e3ac148a2fb577cecbac278770d7aecc4
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-11-28 11:32:53 +0600

    added a transactions table in the database

--- src/main/java/com/mindreader007/nsucash/database/SetupDatabase.java
+++ src/main/java/com/mindreader007/nsucash/database/SetupDatabase.java
@@ -10,10 +10,19 @@ public class SetupDatabase {
                 "password TEXT" +
                 ");";
 
+        String sqltransaction = "CREATE TABLE IF NOT EXISTS transactions (" +
+                "transaction_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "username TEXT NOT NULL," +
+                "date_time TEXT NOT NULL," +
+                "type TEXT NOT NULL CHECK(type IN ('food', 'bus', 'advising', 'transfer', 'book'))," +
+                "value REAL NOT NULL" +
+                ");";
+
         try (Connection conn = Database.connect();
              var stmt = conn.createStatement()) {
 
             stmt.execute(sql);
+            stmt.execute(sqltransaction);
         } catch (Exception e) {
             e.printStackTrace();
         }
