7874a1bd932131b8af19d5447ba0f122d489e296
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-12-09 16:34:47 +0600

    Added A NSUCash Logo

--- src/main/java/com/mindreader007/nsucash/Main.java
+++ src/main/java/com/mindreader007/nsucash/Main.java
@@ -14,6 +14,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -32,6 +33,7 @@ public class Main extends Application {
 
         primaryStage.setTitle("NSUCash");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("image/NSUCash.jpg"))));
         primaryStage.show();
     }
 
