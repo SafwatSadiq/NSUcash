5c9e6075d552784902f9fbaea48c978ce4a1ab8e
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-12-09 16:03:42 +0600

    Created a Booking Model

---
+++ src/main/java/com/mindreader007/nsucash/model/Booking.java
@@ -0,0 +1,58 @@
+package com.mindreader007.nsucash.model;
+
+public class Booking {
+    private int bookingId;
+    private int userId;
+    private int scheduleId;
+    private String bookingTime;
+    private String busName;
+    private String direction;
+    private String stopTimes;
+    private String stops;
+
+    public Booking(int bookingId, int userId, int scheduleId, String bookingTime,
+                   String busName, String direction, String stopTimes, String stops) {
+        this.bookingId = bookingId;
+        this.userId = userId;
+        this.scheduleId = scheduleId;
+        this.bookingTime = bookingTime;
+        this.busName = busName;
+        this.direction = direction;
+        this.stopTimes = stopTimes;
+        this.stops = stops;
+    }
+
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public String getBookingTime() {
+        return bookingTime;
+    }
+
+    public String getBusName() {
+        return busName;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getStopTimes() {
+        return stopTimes;
+    }
+
+    public String getStops() {
+        return stops;
+    }
+}
+
