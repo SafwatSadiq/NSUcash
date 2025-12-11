1cece567ff49c3e06138a9daad1edc271ec2d4ac
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-12-11 18:50:45 +0600

    Created the ticket Ui and gived all tables a fxid

--- src/main/java/com/mindreader007/nsucash/controller/TicketScreen.java
+++ src/main/java/com/mindreader007/nsucash/controller/TicketScreen.java
@@ -1,4 +1,37 @@
 package com.mindreader007.nsucash.controller;
 
-public class TicketScreen {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TicketScreen implements Initializable {
+    @FXML
+    private Label ticketLabel;
+    @FXML
+    private Label bookingIdLabel;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label userIdLabel;
+    @FXML
+    private Label scheduleIdLabel;
+    @FXML
+    private Label bookingTimeLabel;
+    @FXML
+    private Label busIdLabel;
+    @FXML
+    private Label stopsLabel;
+    @FXML
+    private Label stopTimesLabel;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
 }
