dfb2d82d7efd1277121826035e3429106337d7ae
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-12-14 01:23:26 +0600

    Added button functionality in AccountScreen

--- src/main/java/com/mindreader007/nsucash/controller/AccountScreen.java
+++ src/main/java/com/mindreader007/nsucash/controller/AccountScreen.java
@@ -1,8 +1,10 @@
 package com.mindreader007.nsucash.controller;
 
+import com.mindreader007.nsucash.model.User;
 import com.mindreader007.nsucash.services.UserSession;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -26,4 +28,55 @@ public class AccountScreen implements Initializable {
         phoneLabel.setText(UserSession.getUser().getPhoneNumber());
         ageLabel.setText(String.valueOf(UserSession.getUser().getAge()));
     }
+
+    @FXML
+    private void onInstagramClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Instagram");
+        alert.setHeaderText(null);
+        alert.setContentText("Instagram ID: @" + UserSession.getUser().getUsername());
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void onFacebookClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Facebook");
+        alert.setHeaderText(null);
+        alert.setContentText("Facebook ID: @" + UserSession.getUser().getUsername());
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void onTelegramClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Telegram");
+        alert.setHeaderText(null);
+        alert.setContentText("Telegram ID: @" + UserSession.getUser().getUsername());
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void onEmailClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Email");
+        alert.setHeaderText(null);
+        alert.setContentText("Email ID: @" + UserSession.getUser().getUsername());
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void onLinkedinClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Linkedin");
+        alert.setHeaderText(null);
+        alert.setContentText("Linkedin ID: @" + UserSession.getUser().getUsername());
+
+        alert.showAndWait();
+    }
+
 }
