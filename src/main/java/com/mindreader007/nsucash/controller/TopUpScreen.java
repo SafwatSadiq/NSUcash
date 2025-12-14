6bcc1ebcbe9fd22135ce2ce3d9bb4680b3fdf75c
SafwatSadiq <Safwatsadiq14@gmail.com>
2025-12-14 22:48:09 +0600

    Created The Top Up Screen

--- src/main/java/com/mindreader007/nsucash/controller/TopUpScreen.java
+++ src/main/java/com/mindreader007/nsucash/controller/TopUpScreen.java
@@ -1,4 +1,16 @@
 package com.mindreader007.nsucash.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class TopUpScreen {
+    @FXML private ComboBox<String> amountComboBox;
+
+    @FXML
+    public void initialize() {
+        amountComboBox.getItems().addAll(
+                "20", "50", "100", "200", "500", "1000", "10000", "50000", "100000"
+        );
+    }
+
 }
